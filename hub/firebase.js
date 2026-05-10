f6e96f61a80208307b5585bbe734163bed40f18c
pointlessproductions2020-beep <pointlessproductions2020@gmail.com>
2026-05-10 16:54:56 +0100

    Update firebase.js

--- hub/firebase.js
+++ hub/firebase.js
@@ -1,2 +1,16 @@
-// Firebase config will go here later
-console.log("Firebase placeholder loaded");
+// --- Firebase Config ---
+const firebaseConfig = {
+  apiKey: "AIzaSyBQU-ZII5ndeK7hbPtxiyq5-WnGw-jVZhc",
+  authDomain: "needawitness-hub.firebaseapp.com",
+  projectId: "needawitness-hub",
+  storageBucket: "needawitness-hub.firebasestorage.app",
+  messagingSenderId: "132865136242",
+  appId: "1:132865136242:web:8f143b64f0be14727d14eb"
+};
+
+// --- Initialize Firebase ---
+firebase.initializeApp(firebaseConfig);
+
+// Firebase services
+const auth = firebase.auth();
+const db = firebase.firestore();
